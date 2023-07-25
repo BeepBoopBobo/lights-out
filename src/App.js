be25b8b1ea4312dcf4246b86af0cfb118a76dd58
Tomáš Marek <marek.tomas.mk@gmail.com>
2023-07-25 18:32:10 +0200

    add: logo, name, icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import GameBoard from './components/GameBoard';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faXmark, faPlus, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faXmark, faPlus, faMagnifyingGlass);
 
 function App() {
   return <>
